bb4b22381a750339a976e84718211cb45e9c289d
oussamaazg <azougaghdeveloper@gmailcom>
2023-01-12 00:46:54 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args){
+        //byte-short-int-long flout,double
+        String name="Oussama";
+        int age =22;
+        System.out.println("name : "+name);
+        System.out.println("age: "+age);
+        age++;
+        System.out.println("new age "+ age);
+    }  
+}
